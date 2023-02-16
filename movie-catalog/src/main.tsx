3cc9286785ee6510b8a834cffa81bf61973954c6
igsub <igsub@hotmail.com>
2023-02-15 21:03:38 -0300

    Base routing and navbar

--- movie-catalog/src/main.tsx
+++ movie-catalog/src/main.tsx
@@ -2,9 +2,31 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Root from './routes/root';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+    children: [
+      {
+        path: "movie/:movieId",
+        element: <>Hello Movie!</>
+      },
+      {
+        path: "favorites",
+        element: <>Hello Favorites!</>
+      }
+    ]
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
